c04bef942473907d18c9ae1e8e240abc97288adc
olgaeichler <50078307@belfastmet.ac.uk>
2024-09-30 14:37:55 +0100

    first commit

---
+++ recipe/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.recipes, name='recipe_list'),
+    path('update_recipe/<int:id>/', views.update_recipe, name='update_recipe'),
+    path('delete_recipe/<int:id>/', views.delete_recipe, name='delete_recipe'),
+    path('api_recipe/v1/', views.api_recipe, name='api_recipe'),
+]
